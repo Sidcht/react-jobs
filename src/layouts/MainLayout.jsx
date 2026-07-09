ef2996eefcd769304d829e9131c21f98c097d99a
Sidcht <veldabu1327@gmail.com>
2026-07-09 15:10:10 +0800

    Remove unused Bootstrap import

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Toast } from "bootstrap";
 const MainLayout = () => {
   return (
     <>
